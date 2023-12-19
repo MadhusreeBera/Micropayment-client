4045c19d1a79e94f3f3df3c36681ead311ac3ad8
Madhusree Bera <madhusree@Madhusrees-MacBook-Pro.local>
2023-12-19 12:06:37 +0530

    included wallet support

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
+import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
+import {Aptos} from '@aptos-labs/ts-sdk'
+import {useWallet} from '@aptos-labs/wallet-adapter-react'
 
 function App() {
+  const aptos = new Aptos();
+  const {account} = useWallet();
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-3xl font-bold m-5">Aptos Micropayment</h1>
+      <WalletSelector />
+      {account && <p>{account.address}</p>}
+      <div className="flex flex-col mt-3 mx-2">
+        <div className="rounded-sm bg-gray-200 w-auto m-1 h-12 flex justify-center items-center cursor-pointer">Start Micropayment</div>
+        <div className="rounded-sm bg-gray-200 w-auto m-1 h-12 flex justify-center items-center cursor-pointer">Receive Micropayment</div>
+        <div className="rounded-sm bg-gray-200 w-auto m-1 h-12 flex justify-center items-center cursor-pointer">Transaction History</div>
+        <div className="rounded-sm bg-gray-200 w-auto m-1 h-12 flex justify-center items-center cursor-pointer">User Profile</div>
+        <div></div>
+      </div>
     </div>
   );
 }
